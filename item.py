09a12ab29b55da48a2637224d6bdeab20329ed69
voidnologo <void.no.logo@icloud.com>
2016-01-14 22:37:40 -0600

    Updated Item.get_item to accept a connection or db_name

--- item.py
+++ item.py
@@ -6,16 +6,15 @@ from utilities import gprint
 class Item():
 
     @staticmethod
-    def get_item(name):
-        con = sqlite3.connect("game.db")
+    def get_item(name, con=None, db_file='game.db'):
+        if not con:
+            con = sqlite3.connect(db_file)
         jsontext = con.execute("select json from items where name=?", (name,)).fetchone()[0]
         con.close()
         d = json.loads(jsontext)
-        d['id'] = id
         return Item(**d)
 
-    def __init__(self, id=0, name=None, description=None):
-        self.id = id
+    def __init__(self, name=None, description=None):
         self.name = name
         self.description = description
 
