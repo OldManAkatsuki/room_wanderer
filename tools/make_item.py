a343ae91a934617feda21ccab7b2b28ae6d281ea
voidnologo <void.no.logo@icloud.com>
2016-03-08 22:33:07 -0600

    refactor confirmation make item

--- tools/make_item.py
+++ tools/make_item.py
@@ -12,18 +12,34 @@ import os
 
 item_path = os.path.relpath('../data/items_new')
 
-file_name = input('File Name: ')
-item_name = input('Item Name: ')
-short_desc = input('Short Description: ')
-long_desc = input('Long Description: ')
 
-data = {
-    'name': item_name,
-    'short_description': short_desc,
-    'long_description': long_desc
-}
+def get_item_info():
+    file_name = input('File Name: ')
+    item_name = input('Item Name: ')
+    short_desc = input('Short Description: ')
+    long_desc = input('Long Description: ')
+    return (
+        file_name,
+        {
+            'name': item_name,
+            'short_description': short_desc,
+            'long_description': long_desc
+        }
+    )
 
-json_data = json.dumps(data)
 
-with open('{}/{}.json'.format(item_path, file_name), 'w') as f:
-    f.write(json_data)
+def confirm_entry(data):
+    print('\n\n')
+    print('The item you entered: ')
+    print(json_data)
+    confirm = None
+    while confirm not in ['Y', 'N']:
+        confirm = input('\nIs this correct? (y/n) >').upper()
+    return confirm == 'Y'
+
+
+file_name, data = get_item_info()
+json_data = json.dumps(data, indent=3)
+if confirm_entry(json_data):
+    with open('{}/{}.json'.format(item_path, file_name), 'w') as f:
+        f.write(json_data)
