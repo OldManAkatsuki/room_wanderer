b76f43e0b493d3b7473a432ba88605e0b3f3af73
OldManAkatsuki <ZEROdmp@yahoo.com>
2016-03-10 23:02:30 -0600

    added menu and menu options

--- tools/make_item.py
+++ tools/make_item.py
@@ -10,7 +10,35 @@
 import json
 import os
 
+
 item_path = os.path.relpath('../data/items_new')
+ITEM_NAMES = []
+ITEMS = {}
+
+def current_items():
+    files = []
+    for (dirpath, dirnames, filenames) in os.walk(item_path):
+        files.extend(filenames)
+        break
+    for path in files: 
+        with open(os.path.join(item_path, path), "r") as f:
+            file_data = json.loads(f.read())
+        ITEMS[file_data["name"]] = (path, file_data)
+    ITEM_NAMES.extend(sorted(ITEMS.keys()))  
+
+
+def show_details():
+    choice = int(input('What item do you want details for? >>'))
+    item = ITEMS[ITEM_NAMES[choice - 1]]
+    print('{:>20}: {}'.format('Name', item[1]['name']))    
+    print('{:>20}: {}'.format('Short Description', item[1]['short_description'])) 
+    print('{:>20}: {}'.format('Long Description',item[1]['long_description']))
+    print('{:>20}: {}'.format('Filename', item[0]))  
+
+
+def display_current_items():
+    for idx, item in enumerate(ITEM_NAMES, 1):
+        print('{}: {}'.format(idx, item))
 
 
 def get_item_info():
@@ -38,8 +66,33 @@ def confirm_entry(data):
     return confirm == 'Y'
 
 
-file_name, data = get_item_info()
-json_data = json.dumps(data, indent=3)
-if confirm_entry(json_data):
-    with open('{}/{}.json'.format(item_path, file_name), 'w') as f:
-        f.write(json_data)
+def make_item():
+    file_name, data = get_item_info()
+    json_data = json.dumps(data, indent=3)
+    if confirm_entry(json_data):
+        with open('{}/{}.json'.format(item_path, file_name), 'w') as f:
+            f.write(json_data)
+
+
+def menu():
+    quit = False
+    while not quit:
+        print('\n\nPlease choose an action from the following list:')
+        for idx, action in enumerate(ACTIONS, 1):
+            print('{}: {}'.format(idx, action[0]))
+        print('Q: Quit')
+        choice = input('Choice >>').upper()
+        if choice == 'Q':
+            break
+        ACTIONS[int(choice) - 1][1]()
+
+
+ACTIONS = [
+    ('List current items', display_current_items),
+    ('Add an item', make_item),
+    ('Show details', show_details),
+]
+
+current_items()
+menu()
+
